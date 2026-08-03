457aa636a714980c3629158f5426acf202eac33f
JetroMobilha <jetromobilha@gmail.com>
2026-08-03 08:57:04 +0100

    enviar site

--- public/flutter_bootstrap.js
+++ public/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"69c8c61792f04cc809dfef0c910414fb9afc06
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2935864909" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "817509726" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
