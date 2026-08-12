f5f9f72e5fddf1391c07a5198850f3b777d89180
JetroMobilha <jetromobilha@gmail.com>
2026-08-12 07:29:40 +0100

    adicionar sessão no servidor

--- config/reverb.php
+++ config/reverb.php
@@ -96,7 +96,7 @@ return [
                 ],
                 'webhooks' => [
                     [
-                        'url' => env('REVERB_WEBHOOK_URL', env('APP_URL', 'http://localhost') . '/api/webhooks/reverb'),
+                        'url' => 'http://127.0.0.1:8080/api/webhooks/reverb',
                         'events' => ['member_added', 'member_removed', 'channel_occupied', 'channel_vacated'],
                     ],
                 ],
