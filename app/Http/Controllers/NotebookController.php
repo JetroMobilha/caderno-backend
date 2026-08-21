d0d781f0c48b27fc8fe09cdb95f808483d84ab26
JetroMobilha <jetromobilha@gmail.com>
2026-08-21 04:31:58 +0100

    adicionar type liny em papel

--- app/Http/Controllers/NotebookController.php
+++ app/Http/Controllers/NotebookController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema; // 🚀 Corrigir erro "Class Schema not found"
 use App\Models\Subject;
 use App\Models\Notebook;
 use App\Models\User;
